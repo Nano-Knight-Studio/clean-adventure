da328bcde6e679625166d22b1db07262779979ec
felipegodoyf <felipegodoyf@outlook.com>
2020-06-04 19:03:55 -0300

    Inimigos andantes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -14,6 +15,10 @@ public class Enemy : MonoBehaviour
 
     public void DetectPlayer (GameObject player)
     {
-        navMeshAgent.SetDestination(player.transform.position);
+        try
+        {
+            navMeshAgent.SetDestination(player.transform.position);
+        }
+        catch (Exception ex) {}
     }
 }
