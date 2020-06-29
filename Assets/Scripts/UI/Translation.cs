05b88fac6ed606b1f7c1cff687a59304ec0b094a
felipegodoyf <felipegodoyf@outlook.com>
2020-06-29 19:05:31 -0300

    Traduções

---
+++ Assets/Scripts/UI/Translation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Translation : MonoBehaviour
+{
+	[SerializeField] private string englishText;
+	[SerializeField] private string portugueseText;
+
+	void Start ()
+	{
+		if (Application.systemLanguage == SystemLanguage.Portuguese)
+		{
+			GetComponent<TextMeshProUGUI>().text = portugueseText;
+		}
+		else
+		{
+			GetComponent<TextMeshProUGUI>().text = englishText;
+		}
+	}
+}
